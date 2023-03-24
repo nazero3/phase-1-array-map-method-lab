1a84f7ed749dae9ddee4e99c75fd8390abbb69fc
nazero3 <nazerrabah1994@gmail.com>
2023-03-24 14:22:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const tutorials = [
-  'what does the this keyword mean?',
+  "what does the this keyword mean?",
   'What is the Constructor OO pattern?',
   'implementing Blockchain Web API',
   'The Test Driven Development Workflow',
@@ -12,5 +12,19 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  const t = tutorials.map(function (ele) {
+    let words = ele.split(" ");
+    let capWords = words.map(
+      (word) => word.at(0).toUpperCase() + word.substring(1)
+    );
+    let sentence = '';
+    for (let i of capWords){
+      sentence += i + ' ';
+    }
+    console.log(sentence);
+    return sentence.substring(0,sentence.length-1);
+  });
+  console.log(t);
+  return t;
+};
+titleCased(tutorials);
